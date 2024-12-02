7d241d953756e67cc82fb5239d2030d8a420eb89
Yuki Watanabe <ukwhatn@gmail.com>
2024-12-02 23:48:27 +0900

    add models

--- db/package/models.py
+++ db/package/models.py
@@ -1,15 +1,74 @@
-from datetime import datetime, UTC
+from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Integer, DateTime, ForeignKey, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import text
 
 from .connection import Base
 
 
-class TemplateModel(Base):
-    __tablename__ = "templates"
+class Guilds(Base):
+    __tablename__ = "guilds"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.now(UTC))
-    updated_at = Column(DateTime, default=datetime.now(UTC))
-    deleted_at = Column(DateTime, nullable=True)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True
+    )
+
+    guild_id: Mapped[int] = mapped_column(
+        BigInteger,
+        unique=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()")
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()"),
+        onupdate=text("now()")
+    )
+
+    # Relationships
+    registered_roles: Mapped[list["RegisteredRoles"]] = relationship(
+        back_populates="guild"
+    )
+
+
+class RegisteredRoles(Base):
+    __tablename__ = "registered_roles"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True
+    )
+
+    role_id: Mapped[int] = mapped_column(
+        BigInteger,
+        unique=True
+    )
+
+    guild_id: Mapped[int] = mapped_column(
+        ForeignKey("guilds.guild_id"),
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()")
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("now()"),
+        onupdate=text("now()")
+    )
+
+    # Relationships
+    guild: Mapped["Guilds"] = relationship(
+        back_populates="registered_roles"
+    )
+
+
